8e29e839a9a4360c5d4e1f5851fac92988646ac8
Meet Pandya <pandyama2@gmail.com>
2020-10-05 23:04:32 -0400

    app icon
    added app icon and removed unused fonts

--- app/src/main/java/com/mp/mistplaychallenge/model/GameCategory.kt
+++ app/src/main/java/com/mp/mistplaychallenge/model/GameCategory.kt
@@ -1,5 +1,13 @@
 package com.mp.mistplaychallenge.model
 
+/**
+ *  GameCategory - data class that containing Game category and list of games
+ *  Data classes act like data holder classes and helps with avoiding to write extra code
+ *
+ *  @param list_title - holds the Game category title
+ *  @param games - List of data type GameDetail containing details of each game in its respective category
+ */
+
 data class GameCategory(
     var list_title: String?,
     var games: List<GameDetail>?
--- app/src/main/java/com/mp/mistplaychallenge/model/GameDetail.kt
+++ app/src/main/java/com/mp/mistplaychallenge/model/GameDetail.kt
@@ -1,5 +1,13 @@
 package com.mp.mistplaychallenge.model
 
+/**
+ *  GameDetail - data class containing game detail info
+ *  Data classes act like data holder classes and helps with avoiding to write extra code
+ *
+ *  @param title - holds the game title
+ *  @param img - holds the URL where the game's image is stored
+ */
+
 data class GameDetail(
     var title: String?,
     var img: String?
